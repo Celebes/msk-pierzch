5de879114b9d931a40d34378dbe0ef4cba4ef5fb
Celebeze <kris1337@gmail.com>
2014-05-25 23:04:27 +0200

    Poprawki w xml/xsd Bramka wstep

---
+++ msk-bpmn-engine/src/pl/edu/wat/msk/bramki_logiczne/BramkaLogiczna.java
@@ -0,0 +1,14 @@
+package pl.edu.wat.msk.bramki_logiczne;
+
+import pl.edu.wat.wcy.mtsk.xml_elements.Symulacja;
+
+public class BramkaLogiczna {
+	
+	private String id;
+	private String rodzaj;
+	
+	public BramkaLogiczna(Symulacja.Czynnosc.Bramka bramkaDaneXML) {
+		
+	}
+
+}
