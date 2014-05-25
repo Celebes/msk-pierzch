515bbf0bacda36237fc14eaa48a0e5e9b9030d48
KRRySS <jedynakpoczta@gmail.com>
2014-05-25 19:25:10 +0200

    Przeniesieni logiki z konstruktora do prywatne metody.

--- msk-bpmn-engine/src/pl/edu/wat/msk/otoczenie/OtoczenieGeneric.java
+++ msk-bpmn-engine/src/pl/edu/wat/msk/otoczenie/OtoczenieGeneric.java
@@ -24,93 +24,98 @@ public class OtoczenieGeneric
     
     public OtoczenieGeneric(String type, String a, String b, String c)
     {
-        type = type.toUpperCase();
-        try
-        {
-            if (type == "BETA") {
-                Integer x = Integer.parseInt(a);
-                double y = Double.parseDouble(b);
-                distribution = new Erlang(x,y);
-            }
-            else if (type == "BINOMIAL") {
-                double x = Double.parseDouble(a);
-                Integer y = Integer.parseInt(b);
-                distribution = new Binomial( x, y );
-            }
-            else if (type == "CHISQUARE") {
-                Integer x = Integer.parseInt(a);
-                distribution = new ChiSquare(x);
-            }
-            else if (type == "ERLANG") {
-                Integer x = Integer.parseInt(a);
-                double y = Double.parseDouble(b);
-                distribution = new Erlang(x,y);
-            }
-            else if (type == "EXPONENTIAL") {
-                double x = Double.parseDouble(a);
-                distribution = new Exponential(x);
-            }
-            else if (type == "GAMMA") {
-                double x = Double.parseDouble(a);
-                double y = Double.parseDouble(b);
-                distribution = new Gamma( x, y );
-            }
-            else if (type == "GEOMETRIC") {
-                double x = Double.parseDouble(a);
-                distribution = new Geometric( x );
-            }
-            else if (type == "LAPLACE") {
-                double x = Double.parseDouble(a);
-                distribution = new Laplace( x );
-            }
-            else if (type == "LOGNORMAL") {
-                double x = Double.parseDouble(a);
-                double y = Double.parseDouble(b);
-                distribution = new LogNormal( x, y );
-            }
-            else if (type == "NEGATIVEBINOMIAN") {
-                double x = Double.parseDouble(a);
-                Integer y = Integer.parseInt(b);
-                distribution = new NegativeBinomial(x,y);
-            }
-            else if (type == "NORMAL") {
-                double x = Double.parseDouble(a);
-                double y = Double.parseDouble(b);
-                distribution = new Normal( x, y );
-            }
-            else if (type == "POISSON") {
-                double x = Double.parseDouble(a);
-                double y = Double.parseDouble(b);
-                distribution = new Poisson( x, y );
-            }
-            else if (type == "STUDENT") {
-                int x = Integer.parseInt(a);
-                distribution = new Student(x);
-            }
-            else if (type == "TRIANGULAR") {
-                double x = Double.parseDouble(a);
-                distribution = new Triangular(x);
-            }
-            else if (type == "UNIFORM") {
-                double x = Double.parseDouble(a);
-                double y = Double.parseDouble(b);
-                distribution = new Uniform( x, y );
-            }
-            else if (type == "WEIBULL") {
-                double x = Double.parseDouble(a);
-                double y = Double.parseDouble(b);
-                distribution = new Weibull( x,y);
-            }
-        }
-        catch ( Exception e )
-        {
-            System.err.println("WPROWADZONO ZŁE DANE!");
-        }
+      this.convertDataForDistribution(type, a, b, c);
     }
     
     
     public double getNextDouble() {
         return distribution.getNextDouble();
+    }
+    
+    
+    private void convertDataForDistribution(String type, String a, String b, String c){
+    	  type = type.toUpperCase();
+          try
+          {
+              if (type == "BETA") {
+                  Integer x = Integer.parseInt(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new Erlang(x,y);
+              }
+              else if (type == "BINOMIAL") {
+                  double x = Double.parseDouble(a);
+                  Integer y = Integer.parseInt(b);
+                  distribution = new Binomial( x, y );
+              }
+              else if (type == "CHISQUARE") {
+                  Integer x = Integer.parseInt(a);
+                  distribution = new ChiSquare(x);
+              }
+              else if (type == "ERLANG") {
+                  Integer x = Integer.parseInt(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new Erlang(x,y);
+              }
+              else if (type == "EXPONENTIAL") {
+                  double x = Double.parseDouble(a);
+                  distribution = new Exponential(x);
+              }
+              else if (type == "GAMMA") {
+                  double x = Double.parseDouble(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new Gamma( x, y );
+              }
+              else if (type == "GEOMETRIC") {
+                  double x = Double.parseDouble(a);
+                  distribution = new Geometric( x );
+              }
+              else if (type == "LAPLACE") {
+                  double x = Double.parseDouble(a);
+                  distribution = new Laplace( x );
+              }
+              else if (type == "LOGNORMAL") {
+                  double x = Double.parseDouble(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new LogNormal( x, y );
+              }
+              else if (type == "NEGATIVEBINOMIAN") {
+                  double x = Double.parseDouble(a);
+                  Integer y = Integer.parseInt(b);
+                  distribution = new NegativeBinomial(x,y);
+              }
+              else if (type == "NORMAL") {
+                  double x = Double.parseDouble(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new Normal( x, y );
+              }
+              else if (type == "POISSON") {
+                  double x = Double.parseDouble(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new Poisson( x, y );
+              }
+              else if (type == "STUDENT") {
+                  int x = Integer.parseInt(a);
+                  distribution = new Student(x);
+              }
+              else if (type == "TRIANGULAR") {
+                  double x = Double.parseDouble(a);
+                  distribution = new Triangular(x);
+              }
+              else if (type == "UNIFORM") {
+                  double x = Double.parseDouble(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new Uniform( x, y );
+              }
+              else if (type == "WEIBULL") {
+                  double x = Double.parseDouble(a);
+                  double y = Double.parseDouble(b);
+                  distribution = new Weibull( x,y);
+              }
+          }
+          catch ( Exception e )
+          {
+              System.err.println("WPROWADZONO ZŁE DANE!");
+          }
     }
 
 }
